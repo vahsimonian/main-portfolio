3986e61784ab785c38fe0aa7928a9a91103acec4
vahsimonian <simonianvah@gmail.com>
2023-11-20 13:13:39 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./navbar.css";
+import letterV from "../assets/letter-v.png";
 
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="navbar">
+      <img src={letterV} width={70} alt="logo" className="logo" />
+      <div className="desktop-menu">Menu</div>
+      <button className="desktop-menu-btn">
+        <img src="" alt="" className="desktop-menu-img" />
+        Contact Me
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar;

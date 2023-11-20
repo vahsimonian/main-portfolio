cfb221a66b79e6a44a52d861422fc3cc8c446a6e
vahsimonian <simonianvah@gmail.com>
2023-11-20 13:22:36 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./navbar.css";
 import letterV from "../assets/letter-v.png";
+import { Link } from "react-scroll";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src={letterV} width={70} alt="logo" className="logo" />
-      <div className="desktop-menu">Menu</div>
+      <img src={letterV} alt="logo" className="logo" />
+      <div className="desktop-menu">
+        <Link classID="desktom-menu-list-item">Home</Link>
+        <Link classID="desktom-menu-list-item">About</Link>
+        <Link classID="desktom-menu-list-item">Customers</Link>
+        <Link classID="desktom-menu-list-item">Portfolio</Link>
+      </div>
       <button className="desktop-menu-btn">
         <img src="" alt="" className="desktop-menu-img" />
         Contact Me

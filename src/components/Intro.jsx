1e2e6a38acf4f73ced66945a5e19ce93bed119c3
vahsimonian <simonianvah@gmail.com>
2023-11-22 10:03:16 +0700

    commit

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,15 +9,17 @@ function Intro() {
     <section id="intro">
       <div className="intro-content">
         <span className="hello"> Hello, </span>
-        <span className="intro-text"> I'm </span>
-        <span className="intr-name"> Vah </span>
-        <br /> Web-developer
+        <span className="intro-text">
+          {" "}
+          I'm <span className="intro-name"> Vah </span>
+          <br /> Web-developer{" "}
+        </span>
         <p className="intro-para">
           I am a React JS web developer with 2.5 years' experience.
         </p>
         <Link>
           <button className="btn">
-            <img src={suitcaseLine} alt="suitcase" />
+            <img src={suitcaseLine} alt="suitcase" className="btn-img" />
             Hire Me
           </button>
         </Link>

8d51308bcb0e665d2d71cf2025e4577992871b18
vahsimonian <simonianvah@gmail.com>
2023-11-24 03:12:10 +0700

    commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,10 +8,30 @@ function Skills() {
   return (
     <section id="skills">
       <span className="skill-title">What I do</span>
+      <br />
       <span className="skill-desc">
-        I am skilled and passionate web-developer / designer
+        I am skilled and passionate web-developer / designer Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Inventore ab in, delectus
+        aspernatur harum quam ipsa nobis quisquam id quas voluptas nihil at
+        assumenda adipisci dolorem quibusdam libero cumque deserunt ducimus
+        dolores.
       </span>
       <div className="skill-bar-container">
+        <div className="skill-bar">
+          <img
+            src={websiteDesign}
+            alt="website-design"
+            className="skill-bar-img"
+            width={60}
+          />
+          <div className="skill-bar-text">
+            <h2>Web-Developer</h2>
+            <p>
+              Developing dynamic, responsive web applications using React JS for
+              optimal performance.
+            </p>
+          </div>
+        </div>
         <div className="skill-bar">
           <img
             src={uiuxDesign}
@@ -19,18 +39,28 @@ function Skills() {
             className="skill-bar-img"
             width={60}
           />
-          <img
-            src={websiteDesign}
-            alt="website-design"
-            className="skill-bar-img"
-            width={60}
-          />
+          <div className="skill-bar-text">
+            <h2>UI / UX design</h2>
+            <p>
+              Creating intuitive, engaging interfaces enhances user experience
+              and satisfaction in design.
+            </p>
+          </div>
+        </div>
+        <div className="skill-bar">
           <img
             src={appDesign}
             alt="app-design"
             className="skill-bar-img"
             width={60}
           />
+          <div className="skill-bar-text">
+            <h2>App-Developer(beginner)</h2>
+            <p>
+              As an app developer, I craft innovative React JS-based mobile
+              applications.
+            </p>
+          </div>
         </div>
       </div>
     </section>

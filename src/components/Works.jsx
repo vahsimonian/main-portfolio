3c3c40cce11084a3b5131ef5d7386263dd8c4da8
vahsimonian <simonianvah@gmail.com>
2023-11-28 15:32:54 +0700

    commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,16 +18,52 @@ function Works() {
         stone unturned in my pursuit of excellence.
       </span>
       <div className="works-images">
-        <img src={burgerShop} alt="burger-shop" className="work-img" />
-        <img src={bitcoinShop} alt="bitcoin-shop" className="work-img" />
-        <img src={houseShop} alt="house-shop" className="work-img" />
-        <img src={phoneShop} alt="phone-shop" className="work-img" />
-        <img
-          src={temperatureShop}
-          alt="temperature-shop"
-          className="work-img"
-        />
-        <img src={vanShop} alt="van-shop" className="work-img" />
+        <a
+          href="https://master--neon-kringle-618c71.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={burgerShop} alt="burger-shop" className="work-img" />
+        </a>
+        <a
+          href="https://master--precious-sprite-7845c0.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={bitcoinShop} alt="bitcoin-shop" className="work-img" />
+        </a>
+        <a
+          href="https://classy-clafoutis-7f38fe.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={houseShop} alt="house-shop" className="work-img" />
+        </a>
+        <a
+          href="https://e-commerse-in-moscow.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={phoneShop} alt="phone-shop" className="work-img" />
+        </a>
+        <a
+          href="https://master--rococo-strudel-89abf2.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={temperatureShop}
+            alt="temperature-shop"
+            className="work-img"
+          />
+        </a>
+        <a
+          href="https://jovial-zabaione-40249a.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={vanShop} alt="van-shop" className="work-img" />
+        </a>
       </div>
       <button className="works-btn">See More</button>
     </section>
